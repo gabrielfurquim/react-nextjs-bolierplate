c874afe2ac5994f34ec3e628ca20f733b5626716
gabrielfurquim <gabriel.furquim@logisticag2l.com.br>
2020-09-03 12:05:41 -0300

    feat: add children props to layout and render the spacex spaceships

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import withApollo from 'graphql/client'
-// import { useQuery } from '@apollo/react-hooks'
+import { useQuery } from '@apollo/react-hooks'
 
-// import GET_SPACEX_ROCKETS from 'graphql/queries/getSpacexRockets'
+import GET_SPACEX_ROCKETS from 'graphql/queries/getSpacexRockets'
 import Layout from 'containers/Layout'
 
+type Rocket = {
+  id: string
+}
+
 function Home() {
-  // const { data } = useQuery(GET_SPACEX_ROCKETS)
-  return <Layout pageTitle="Home"></Layout>
+  const { data } = useQuery(GET_SPACEX_ROCKETS)
+
+  return (
+    <>
+      <Layout pageTitle="Home">
+        <ul>
+          {data &&
+            data.rockets.map((rocket: Rocket) => (
+              <li key={rocket.id}>{rocket.id}</li>
+            ))}
+        </ul>
+      </Layout>
+    </>
+  )
 }
 
 export default withApollo({ ssr: true })(Home)
